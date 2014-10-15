aeded5080e6e339e7432384dc810230d59068e11
Vered Bauer <veredbauer@Vereds-MacBook-Air.local>
2014-10-15 12:56:21 -0500

    form testing

--- spec/requests/layout_linkes_spec.rb
+++ spec/requests/layout_linkes_spec.rb
@@ -14,3 +14,16 @@ describe "LayoutLinks", type: 'view' do
         response.should have_selector('title', :content => "Hello Page")
     end
 end
+
+
+describe "failure" do
+   it "should not give results" do
+      lambda do
+         visit pages_hello_path
+         fill_in :start_time,    :with => ""
+         fill_in :end_time,      :with => ""
+         click_button
+         response.should render_template('pages/hello')
+      end
+   end
+end
