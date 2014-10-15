356bc2875453f5356096f82f26da898a416de079
Vered Bauer <veredbauer@Vereds-MacBook-Air.local>
2014-10-15 10:50:50 -0500

    Rspec added to Gemfile and first test added

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe PagesController do
+
+  describe "GET 'hello'" do
+    it "returns http success" do
+      get 'hello'
+      response.should be_success
+    end
+  end
+end
