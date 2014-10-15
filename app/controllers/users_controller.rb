733608570fd94842872e95231df617ed295d8a83
Vered Bauer <veredbauer@Vereds-MacBook-Air.local>
2014-10-15 09:59:47 -0500

    taking out irrelevant code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,7 @@
 class UsersController < ApplicationController
   def new
-      @user = User.new()
   end
   def create
-      @user = User.create(user_params)
-      if @user.save
-         redirect_to '/index'
-      else
-         render 'new'
-      end
   end
 
   def show
@@ -20,12 +13,5 @@ class UsersController < ApplicationController
   def update
   end
   def index
-      @user = User.new
-      @users = User.all
   end
 
-private
-     def user_params
-         params.require(:user).permit(:name, :email)
-     end
-end
