4533e84a400ec2897390ab8e665e27200dac0abc
Vered Bauer <veredbauer@Vereds-MacBook-Air.local>
2014-10-15 12:11:27 -0500

    integration tests for routes added

--- spec/requests/layout_linkes_spec.rb
+++ spec/requests/layout_linkes_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 describe "LayoutLinks", type: 'view' do
+
+
+    it "should have a hello page at '/'" do
+        get '/'
+        response.should have_selector('title', :content => "Hello Page")
+    end
+
     
     it "should have a hello page at 'pages/hello'" do
         get 'pages/hello'
